a26face2ddd240b50f223b5e24c7e68e43ed27bd
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-12 20:26:27 +0100

    Usando Colum ao invez de Container.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter/material.dart';
 void main() {
 //Funcao para Executar o app
   runApp(MaterialApp(
-  //Titulo
-  title: "Frases do Dia",
-  //Container Principal
-  home: Container(color: Colors.white,),
+    //Titulo
+    title: "Frases do Dia",
+    //Container Principal
+    //home: Container(color: Colors.white,),
+    home: Column(
+      children: <Widget>[
+        
+      ],
+    ),
   ));
 }
