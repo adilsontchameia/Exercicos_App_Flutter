e1f97312405aa15b70d5d4d8e7ba1d894d5d13e3
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-13 13:04:38 +0100

    Insersao de imagens

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,10 @@ void main() {
     //Container Principal
     home: Container(
       //color: Colors.white,
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0), //Espacamento interno
-      margin: EdgeInsets.all(20),
-      //margin: EdgeInsets.fromLTRB(10, 10, 0, 0), //Espacamento externo
-      decoration: BoxDecoration(
-        border: Border.all(width: 3, color: Colors.white)
-      ),
-      child: Column(
-        children: <Widget>[
-          /*Text(
-            "Adilson Kamati Chameia, mobile developer from Angola, Menongue",
-          textAlign: TextAlign.justify,) */
-          Text("t1"),
-          Padding(padding: EdgeInsets.all(10),
-          child: Text("Chameia"),),
-          Text("t1")
-        ],
-      ),
+      margin: EdgeInsets.only(top: 40), //Espacamento interno
+      decoration:
+          BoxDecoration(border: Border.all(width: 3, color: Colors.white)),
+      //child: ,
     ),
   ));
 }
